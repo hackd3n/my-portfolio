08b440551b091d8fe34a2a91d26e296c35d002ca
hackd3n <dh@plantec.io>
2023-06-07 13:26:02 +0100

    added previous navigation to contact page

--- my-web-portfolio/src/components/Contact.js
+++ my-web-portfolio/src/components/Contact.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { useParams, Link, useNavigate } from 'react-router-dom';
 function Contact() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -20,9 +20,15 @@ function Contact() {
   };
 
   return (
-    <div className="flex m-auto p-10">
-<iframe className='w-[350px] h-[450px] border-2' src="https://cdn.forms-content.sg-form.com/968d4f15-00a7-11ee-8de0-2a608b412f57"/>
+    <div className="flex m-auto p-10 items-center">
+            <iframe className='w-[350px] h-[450px] border-2' src="https://cdn.forms-content.sg-form.com/968d4f15-00a7-11ee-8de0-2a608b412f57"/>
+<div className="flex text-center px-8">
+        <Link className="mx-auto  bg-black text-white underline transition-color duration-300 hover:bg-customGreen" to='/'>
+          go back
+        </Link>
+      </div>
     </div>
+    
   );
 }
 
