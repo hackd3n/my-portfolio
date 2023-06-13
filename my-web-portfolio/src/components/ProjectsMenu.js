b986de00e66ff66671e9bc571a6bad0850fdd56d
hackd3n <dh@plantec.io>
2023-06-13 14:58:24 +0100

    removed subject view (only)

--- my-web-portfolio/src/components/ProjectsMenu.js
+++ my-web-portfolio/src/components/ProjectsMenu.js
@@ -31,42 +31,20 @@ function ProjectsMenu({ onItemClick }) {
           <span className='text-effect'>The</span> Projects
         </h1>
         <p className='m-auto block text-sm'>
-          The projects I have participated in have been creative. Following music, games, design, beauty, wellness
+          This projects are both personal and work projects utilising my skills in design, web and software developement.
         </p>
 
         <nav>
           <div className='m-auto w-full border-t-2 border-black mt-4 inline-flex place-content-center pb-2 max-h-[200px]'>
             <img src={arrowLeft} alt='arrow pointing right' className='w-[25px]' />
-            <ul className='w-full gap-4 grid grid-rows-3 p-4 justify-center items-center'>
-              <li className={`m-auto ${activeIndex !== -1 ? 'hover:bg-white hover:text-black hover:no-underline' : 'bg-white'}`}>
-                <button className={`mx-auto ${activeIndex !== -1 ? 'hidden' : ''}`} onClick={() => toggleSubList(0)}>
-                  ux/ui design
-                </button>
-                <ul className={activeIndex === 0 ? 'block space-y-2.5' : 'hidden'}>
-                  <li>
-                    <Link to='/project/axs-signature'>
-                      AXS - Signature
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/project/diversity-ability'>
-                      Diversity and Ability
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/project/partum-es'>
-                      partum_es
-                    </Link>
-                  </li>
-                </ul>
-                {activeIndex === 0 && <button onClick={resetProjectView}>Return</button>}
-              </li>
+            <ul className='w-full  p-4 justify-center items-center space-y-2'>
 
-              <li className={`m-auto ${activeIndex !== -1 ? 'hover:bg-white hover:text-black hover:no-underline' : 'bg-white'}`}>
-                <button className={`mx-auto ${activeIndex !== -1 ? 'hidden' : ''}`} onClick={() => toggleSubList(1)}>
-                  web & software development
-                </button>
-                <ul className={activeIndex === 1 ? 'block' : 'hidden'}>
+                  <li>
+                    <a href='https://denishackett.com/work/partumes'>
+                      partum_es
+                    </a>
+                  </li>
+
                   <li>
                     <Link to='/project/axs-signature'>
                       AXS - Signature
@@ -74,40 +52,20 @@ function ProjectsMenu({ onItemClick }) {
                   </li>
                   <li>
                     <Link to='/project/learner-portal'>
-                      Diversity and Ability - Learner Portal
+                      Diversity and Ability
                     </Link>
                   </li>
-                </ul>
-                {activeIndex === 1 && <button onClick={resetProjectView}>Return</button>}
-              </li>
-
-              <li className={`m-auto ${activeIndex !== -1 ? 'hover:bg-white hover:text-black hover:no-underline' : 'bg-white'}`}>
-                <button className={`mx-auto ${activeIndex !== -1 ? 'hidden' : ''}`} onClick={() => toggleSubList(2)}>
-                  physical computing
-                </button>
-                <ul className={activeIndex === 2 ? 'block' : 'hidden'}>
+        
                   <li>
                     <a href='https://medium.com/@dhack001/developing-a-stem-player-74781b9f5739' >
                       Stem Player
                     </a>
                   </li>
-                </ul>
-                {activeIndex === 2 && <button onClick={resetProjectView}>Return</button>}
-              </li>
-
-              <li className={`m-auto ${activeIndex !== -1 ? 'hover:bg-white hover:text-black hover:no-underline' : 'bg-white'}`}>
-                <button className={`mx-auto ${activeIndex !== -1 ? 'hidden' : ''}`} onClick={() => toggleSubList(3)}>
-                  iOS Development
-                </button>
-                <ul className={activeIndex === 3 ? 'block' : 'hidden'}>
                   <li>
                     <Link to='/project/lyre'>
-                      Lyre - dynamic music consumption
+                      Lyre
                     </Link>
                   </li>
-                </ul>
-                {activeIndex === 3 && <button onClick={resetProjectView}>Return</button>}
-              </li>
             </ul>
             <img src={arrowRight} alt='arrow pointing left' className='w-[25px]' />
           </div>
